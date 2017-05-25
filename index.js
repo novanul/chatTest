e2347f358db8a7f878c3fa64012066e0e8d79efa
novanul <2698955794@qq.com>
2017-05-25 20:56:00 +0800

    new js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
     console.log('a user connected');
     socket.on('chat message', function(msg){
-        console.log('msg');
         io.emit('chat message', msg);
   });
 });
