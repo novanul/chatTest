22cd6260cca34b5ff0f96f6cfce2ee848c813cbe
novanul <2698955794@qq.com>
2017-05-25 20:55:26 +0800

    v1.1

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.htm');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    console.log('a user connected');
+    socket.on('chat message', function(msg){
+        console.log('msg');
+        io.emit('chat message', msg);
   });
 });
 
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
 
